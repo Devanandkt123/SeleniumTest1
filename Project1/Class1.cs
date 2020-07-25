8a8c5b21218f57df862d30ce093f5dc0e754503a
Devanandkt123 <68769429+Devanandkt123@users.noreply.github.com>
2020-07-26 02:21:10 +0530

    adding files to repo

---
+++ Project1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    [TestFixture]
+    public class Class1
+    {
+        IWebDriver driver = new ChromeDriver(@"C:\Users\LENOVO\Downloads\chromedriver_win32 (1)");
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("Https://WWW.Google.Com");
+
+        }
+        [Test]
+        public void Test1()
+        {
+            IWebElement searchtextbox = driver.FindElement(By.Name("q"));
+            searchtextbox.SendKeys("Selenium by C#");
+            searchtextbox.SendKeys(Keys.Enter);
+            var title = driver.Title.ToString();
+            Assert.AreEqual("Selenium by C# - Google Search", title);
+            Console.WriteLine("Google search done successfully!!");
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+    }
+}
